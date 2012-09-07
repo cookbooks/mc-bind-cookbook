25a4d87c8870a99c2678177f64bc3af6d1a4022c
Blake Irvin <b.irvin@modcloth.com>
2012-09-07 16:49:09 -0400

    WIP

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-set_unless[:dynamic_dns][:tsig_key]  = "/etc/dyndns_keys/tsig.key"
-set_unless[:dynamic_dns][:tsig_private]  = "/etc/dyndns_keys/tsig.private"
+default[:dynamic_dns][:nameserver] = "ns0"
+default[:dynamic_dns][:dns_zone]  = `hostname |cut -d . -f2-9`
+default[:dynamic_dns][:tsig_key]  = "/etc/dyndns_keys/tsig.key"
+default[:dynamic_dns][:tsig_private]  = "/etc/dyndns_keys/tsig.private"
